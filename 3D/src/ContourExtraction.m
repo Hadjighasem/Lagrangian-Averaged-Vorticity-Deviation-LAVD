abecfe8c58222fc39e47ba199a8043f0cc50f6ff
Hadjighasem <alirezah@ethz.ch>
2015-11-15 19:20:15 +0800

    file

--- 3D/src/ContourExtraction.m
+++ 3D/src/ContourExtraction.m
@@ -33,7 +33,7 @@ function bnd = ContourExtraction(VMatrix,x,y,Nct,MinLength,DeficiencyThresh)
 h1 = contourc(x,y,VMatrix,Nct);
 s = getcontourlines(h1);
 disp(sprintf('... %3d contours are extracted.',numel(s)));
-ct_x = [];   ct_y = [];   ct_val = [];
+bnd = struct('xc',[],'yc',[],'cval',[],'xp',[],'yp',[],'valp',[]);
 for k=1:numel(s)
     % check if the contour is closed
     if ( s(k).x(1)==s(k).x(end) ) && ( s(k).y(1)==s(k).y(end) ) 
@@ -41,48 +41,49 @@ for k=1:numel(s)
         if Length>MinLength
             % Check if the contour is convex
             if IsContourConvex(s(k).x,s(k).y,DeficiencyThresh) 
-                ct_x{end+1} = s(k).x';
-                ct_y{end+1} = s(k).y'; 
-                ct_val{end+1} = s(k).v';
+                bnd.xc{end+1} = s(k).x';
+                bnd.yc{end+1} = s(k).y'; 
+                bnd.cval(end+1,1) = s(k).v';
             end
         end
     end
 end
-%% Step2: Selecting the outermost contour for each nested family of contours
-Nct_filt = numel(ct_x);
-disp(sprintf('... %3d contours are closed and convex.',Nct_filt));
-
-% selecting the first point of each contour as a query point
-if Nct_filt~=0
-    xq = cellfun(@(x) x(1),ct_x,'UniformoutPut',true);  
-    yq = cellfun(@(x) x(1),ct_y,'UniformoutPut',true);  
-end
-
-%%
-indEliminte_max = false(Nct_filt,1);
-for ii=1:Nct_filt
-    in = inpolygon(xq,yq,ct_x{ii},ct_y{ii});  in(ii)=0;
-    indEliminte_max(in) = 1;
-end
-ct_x = ct_x(~indEliminte_max);
-ct_y = ct_y(~indEliminte_max);
-ct_val = ct_val(~indEliminte_max);
-
-%% Step3: Finding vortex centers
-%%% Step1: extracting local maxima and minima of the field
+Nct_filt1 = numel(bnd.xc);
+disp(sprintf('... %3d contours are closed and convex.',Nct_filt1));
+%% Step2: Finding local maxima of the LAVD field.
 [xp,yp,valp] = Find2DPeak(VMatrix,x,y,'maxima');
-%%% Step2: Finding associated minima or maxima of each contour
-bnd = struct('xc',[],'yc',[],'cval',[],'xp',[],'yp',[],'valp',[]);
-for ii=1:numel(ct_x)
-    ind_in = find( inpolygon(xp,yp,ct_x{ii},ct_y{ii}) );
-    [~,indmax] = max(valp(ind_in));
-    indmax = ind_in(indmax);
-    if isempty(indmax); continue; end;
-    bnd.xc{end+1} = ct_x{ii};
-    bnd.yc{end+1} = ct_y{ii};
-    bnd.cval(end+1,1) = ct_val{ii};
-    bnd.xp(end+1,1) = xp(indmax);
-    bnd.yp(end+1,1) = yp(indmax);
-    bnd.valp(end+1,1) = valp(indmax);
-end
+%- Keeping closed contours that encompass only "1" local maximum.
+InMax = cellfun(@(x,y) inpolygon(xp,yp,x,y),bnd.xc,bnd.yc,'UniformoutPut',false);
+InMax = cat(1,InMax{:});       % rows --> closed contours & columns --> local maxima
+N_InMax = sum(InMax,2);        % Number of local maxima in each contour
+indEliminate_1 = N_InMax~=1;
+bnd.xc(indEliminate_1)   = [];
+bnd.yc(indEliminate_1)   = [];
+bnd.cval(indEliminate_1) = [];
 
+[indmax,~] = find( InMax(~indEliminate_1,:)'==1 );
+bnd.xp = xp(indmax);
+bnd.yp = yp(indmax);
+bnd.valp = valp(indmax);
+%% Step3: Selecting the outermost contour for each nested family of contours
+%- selecting the first point of each contour as a query point
+Nct_filt2 = numel(bnd.xc);
+if Nct_filt2~=0
+    xq = cellfun(@(x) x(1),bnd.xc,'UniformoutPut',true);  
+    yq = cellfun(@(x) x(1),bnd.yc,'UniformoutPut',true);  
+end
+%%
+indEliminate_2 = false(Nct_filt2,1);
+for ii=1:Nct_filt2
+    in = inpolygon(xq,yq,bnd.xc{ii},bnd.yc{ii});  in(ii)=0;
+    indEliminate_2(in) = 1;
+end
+bnd.xc(indEliminate_2)   = [];
+bnd.yc(indEliminate_2)   = [];
+bnd.cval(indEliminate_2) = [];
+
+bnd.xp(indEliminate_2)   = [];
+bnd.yp(indEliminate_2)   = [];
+bnd.valp(indEliminate_2) = [];
+
+end
